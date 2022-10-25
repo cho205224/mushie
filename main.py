c721de45c7fd309f7e956675a608f874bc32e683
cho205224 <116055612+cho205224@users.noreply.github.com>
2022-10-24 20:47:21 -0700

    Initialize application

---
+++ main.py
@@ -0,0 +1,34 @@
+from kivy.config import Config
+
+Config.set('graphics', 'width', '900')
+Config.set('graphics', 'height', '400')
+
+from kivy import platform
+from kivy.core.window import Window
+from kivy.app import App
+from kivy.graphics import Color, Line, Quad
+from kivy.properties import NumericProperty, Clock
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    def __init__(self, **kwargs):
+        super(MainWidget, self).__init__(**kwargs)
+
+        if self.is_desktop():
+            self.keyboard = Window.request_keyboard(self.keyboard_closed, self)
+            self.keyboard.bind(on_key_down=self.on_keyboard_down)
+            self.keyboard.bind(on_key_up=self.on_keyboard_up)
+            Clock.schedule_interval(self.update, 1.0 / 60.0)
+
+    def is_desktop(self):
+        if platform in ('linux', 'win', 'macosx'):
+            return True
+        return False
+
+
+class MushieApp(App):
+    pass
+
+
+MushieApp().run()
