a8f9be8c26b575bd3c83007d0b001c39244c3d54
cho205224 <116055612+cho205224@users.noreply.github.com>
2022-11-03 07:22:55 -0700

    game state/main menu
    Main starting menu added.
    Game over and game start added.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.config import Config
+from kivy.lang import Builder
 from kivy.metrics import dp
+
 
 Config.set('graphics', 'width', '900')
 Config.set('graphics', 'height', '400')
@@ -8,17 +10,26 @@ from kivy import platform
 from kivy.core.window import Window
 from kivy.app import App
 from kivy.graphics import Color, Ellipse, Rectangle
-from kivy.properties import Clock
-from kivy.uix.widget import Widget
+from kivy.properties import Clock, ObjectProperty
+from kivy.uix.relativelayout import RelativeLayout
 
+Builder.load_file("menu.kv")
 
-class MainWidget(Widget):
+class MainWidget(RelativeLayout):
     from user_actions import on_touch_down, on_touch_move, on_touch_up
+
+    menu_widget = ObjectProperty()
 
     finger_size = 40
     mushie_size = 40
-    speed_x = 3
-    speed_y = 3
+
+    speed = 1
+    speed_x = speed
+    speed_y = speed
+
+    lives = 3
+    game_over = False
+    game_start = False
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -26,9 +37,11 @@ class MainWidget(Widget):
         if self.is_desktop():
             Clock.schedule_interval(self.update, 1.0 / 60.0)  # 60fps, ideally utilize dt later
 
-        with self.canvas:  # generate mushie (add sprite later instead of ellipse)
+        with self.canvas:
+            # generate mushie (add sprite later instead of ellipse)
             self.mushie = Ellipse(pos=self.center, size=(dp(self.mushie_size), dp(self.mushie_size)))
 
+            # generate finger (add sprite later instead of rect)
             Color(0, 1, 0)
             self.finger = Rectangle(pos=(100, 100), size=(dp(self.finger_size), dp(self.finger_size)))
 
@@ -40,22 +53,31 @@ class MainWidget(Widget):
         finger_pos_x = self.finger.pos[0]
         finger_pos_y = self.finger.pos[1]
         x, y = self.mushie.pos
-        x += self.speed_x
-        y += self.speed_y
+        if self.game_start:
+            x += self.speed_x
+            y += self.speed_y
 
         if y + self.mushie_size > self.height:  # bounce on y-axis wall (Change to game over later)
             y = self.height - self.mushie_size
             self.speed_y = -self.speed_y
+            self.lives -= 1
+            print(self.lives)
         if y < 0:
             y = 0
             self.speed_y = -self.speed_y
+            self.lives -= 1
+            print(self.lives)
 
         if x + self.mushie_size > self.width:  # bounce on x-axis wall (Change to game over later)
             x = self.width - self.mushie_size
             self.speed_x = -self.speed_x
+            self.lives -= 1
+            print(self.lives)
         if x < 0:
             x = 0
             self.speed_x = -self.speed_x
+            self.lives -= 1
+            print(self.lives)
 
         # bounce off finger bottom
         if (y + self.mushie_size > finger_pos_y) \
@@ -89,12 +111,21 @@ class MainWidget(Widget):
             x = finger_pos_x + self.finger_size
             self.speed_x = -self.speed_x
 
-        self.mushie.pos = (x, y)  # update mushie pos
+        if not self.game_over:
+            self.mushie.pos = (x, y)  # update mushie pos
+
+        if self.lives == 0 and not self.game_over:
+            self.game_over = True
+            print("Game Over")
 
     def is_desktop(self):
         if platform in ('linux', 'win', 'macosx'):
             return True
         return False
+
+    def start_button(self):
+        self.game_start = True
+        self.menu_widget.opacity = 0
 
 
 class MushieApp(App):
--- user_actions.py
+++ user_actions.py
@@ -1,6 +1,11 @@
+from kivy.uix.relativelayout import RelativeLayout
+
+
 def on_touch_down(self, touch):
-    self.finger.pos = (touch.pos[0] - (self.finger_size / 2),
-                       touch.pos[1] - (self.finger_size / 2))
+    if not self.game_over and self.game_start:
+        self.finger.pos = (touch.pos[0] - (self.finger_size / 2),
+                           touch.pos[1] - (self.finger_size / 2))
+    return super(RelativeLayout, self).on_touch_down(touch)
 
 
 def on_touch_move(self, touch):
